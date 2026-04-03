bff4ab928e87db89a53ae65d6e2608f5ecab0747
宇阳 <liuyuyang1024@yeah.net>
2026-04-03 22:40:58 +0800

    refactor: 更新SwiperController和SwiperService以增强一致性和可读性
    - 修改SwiperController中的方法签名，使用SwiperFormDTO和SwiperVO替代Swiper，提升数据传输对象的清晰度。
    - 在SwiperService中新增方法以处理Swiper的添加、删除和编辑逻辑，简化控制器中的业务逻辑。
    - 更新获取轮播图列表的逻辑，支持分页和过滤，提升接口的灵活性和可用性。

--- blog/src/main/java/liuyuyang/net/web/service/SwiperService.java
+++ blog/src/main/java/liuyuyang/net/web/service/SwiperService.java
@@ -2,9 +2,23 @@ package liuyuyang.net.web.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import liuyuyang.net.dto.swiper.SwiperFilterDTO;
+import liuyuyang.net.dto.swiper.SwiperFormDTO;
 import liuyuyang.net.model.Swiper;
-import liuyuyang.net.dto.PageDTO;
+import liuyuyang.net.vo.swiper.SwiperVO;
+
+import java.util.List;
 
 public interface SwiperService extends IService<Swiper> {
-    Page<Swiper> getSwiperList(PageDTO pageDTO);
+    void addSwiperData(SwiperFormDTO swiperFormDTO);
+
+    void delSwiperData(Integer id);
+
+    void batchDelSwiperData(List<Integer> ids);
+
+    void editSwiperData(SwiperFormDTO swiperFormDTO);
+
+    SwiperVO getSwiperData(Integer id);
+
+    Page<SwiperVO> getSwiperList(SwiperFilterDTO swiperFilterDTO);
 }
--- model/src/main/java/liuyuyang/net/dto/cate/CateFilterDTO.java
+++ model/src/main/java/liuyuyang/net/dto/cate/CateFilterDTO.java
@@ -4,8 +4,10 @@ import io.swagger.annotations.ApiParam;
 import liuyuyang.net.dto.PageDTO;
 import liuyuyang.net.enums.cate.CatePatternEnum;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class CateFilterDTO extends PageDTO {
     @ApiParam(value = "展示模式：tree 树形结构（默认），list 列表结构")
     private CatePatternEnum pattern;
