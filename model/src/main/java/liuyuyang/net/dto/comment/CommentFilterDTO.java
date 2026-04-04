64ebee7c830203a1be0c4821cc15c59a4aaba9d3
宇阳 <liuyuyang1024@yeah.net>
2026-04-04 16:24:08 +0800

    refactor: 移除不必要的关键字过滤逻辑以简化查询
    - 在CommonUtils、ArticleServiceImpl和FootprintServiceImpl中移除基于关键字的过滤逻辑，提升代码清晰度。
    - 更新FilterDTO和相关DTO，去除不再使用的关键字字段，增强数据传输对象的一致性。
    - 修改CommentServiceImpl中的内容过滤逻辑，简化条件判断，提升可读性。

--- model/src/main/java/liuyuyang/net/dto/comment/CommentFilterDTO.java
+++ model/src/main/java/liuyuyang/net/dto/comment/CommentFilterDTO.java
@@ -14,6 +14,6 @@ public class CommentFilterDTO extends FilterDTO {
     @ApiModelProperty(value = "评论状态：0 待审核（默认），1 审核通过")
     private Integer status = 1;
 
-    @ApiModelProperty(value = "内容关键词筛选")
+    @ApiModelProperty(value = "根据评论内容模糊查询")
     private String content;
 }
--- model/src/main/java/liuyuyang/net/vo/cate/CateVO.java
+++ model/src/main/java/liuyuyang/net/vo/cate/CateVO.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import io.swagger.annotations.ApiModelProperty;
 import liuyuyang.net.model.Cate;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class CateVO extends Cate {
     @TableField(exist = false)
     @ApiModelProperty(value = "该分类下文章数量", example = "10")
