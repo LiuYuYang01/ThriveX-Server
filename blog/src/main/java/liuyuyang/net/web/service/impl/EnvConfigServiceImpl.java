d12997e1a7a2b9f1b05d9d958e4be9f43d4a8cd0
刘宇阳 <liuyuyang1024@yeah.net>
2026-09-17 11:02:03 +0800

    refactor(env): 导入缺失的HashMap类
    补全EnvConfigServiceImpl中缺失的HashMap依赖导入

--- blog/src/main/java/liuyuyang/net/web/service/impl/EnvConfigServiceImpl.java
+++ blog/src/main/java/liuyuyang/net/web/service/impl/EnvConfigServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
