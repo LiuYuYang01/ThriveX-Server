eb47ff3b018f4d3335613ac4c5c29a6ab0733825
宇阳 <liuyuyang1024@yeah.net>
2026-09-20 18:28:38 +0800

    refactor(model): 替换Swagger注解为Schema注解
    将PageDTO类的@Parameter注解替换为@Schema注解，统一接口文档注解规范，优化参数描述与默认值配置

--- model/src/main/java/liuyuyang/net/dto/PageDTO.java
+++ model/src/main/java/liuyuyang/net/dto/PageDTO.java
@@ -1,12 +1,12 @@
 package liuyuyang.net.dto;
 
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class PageDTO {
-    @Parameter(description = "页码：默认第 1 页，不传则返回全部")
+    @Schema(description = "页码", defaultValue = "1")
     private Integer pageNum;
-    @Parameter(description = "页数：默认每页 5 条，不传则返回全部")
+    @Schema(description = "每页条数", defaultValue = "5")
     private Integer pageSize;
 }
